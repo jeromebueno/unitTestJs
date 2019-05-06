cf702ddaa3cd8a6e004235240e04394dfb1e4908
jerome <jeromebueno55@gmail.com>
2019-05-06 21:17:35 +0200

    Exchange mock

--- test/exchange.test.js
+++ test/exchange.test.js
@@ -2,21 +2,37 @@ const Exchange  = require('../exchange.js');
 const User   = require ('../user.js');
 const Product   = require ('../product.js');
 
+jest.mock('../service/database.js')
+const mockDatabase = require('../service/database.js')
+const dMock = jest.fn()
+mockDatabase.mockImplementation(() => {
+  return {
+  save:dMock
+  }
+});
+
+jest.mock('../service/mailer.js')
+const mockMailer = require('../service/mailer.js');
+const mMock = jest.fn();
+mockMailer.mockImplementation(() => {
+  return {
+  send:mMock
+  }
+});
+
+let mailer = new mockMailer()
+let database = new mockDatabase()
+
 let minorReceiver = new User('jerome.bueno@hotmail.fr','jerome','bueno','10');
 let owner = new User('test.owner@hotmail.fr','test','owner','17');
 let product = new Product('mail',owner)
 let exchange = new Exchange(minorReceiver,product,'2019-05-30','2019-05-30')
 
-jest.mock('../service/database.js')
-jest.mock('../service/mailer.js')
-const Mailer = require('../service/mailer.js');
-Mailer.mockImplementation(() => {
-  console.log('Test mock class Mail')
-})
-
 describe('When receiver is minor', () => {
   it('Should return true after send mail', () => {
-    expect(exchange.save()).toEqual(true);
+    exchange.save()
+    expect(mailer.send).toHaveBeenCalled();
+    expect(database.save).not.toHaveBeenCalled();
   });
 });
 
